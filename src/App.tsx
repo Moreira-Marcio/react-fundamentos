af0727eaebed5c41ff22b6dd170c72ee201accf1
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-06-23 09:55:40 -0300

    adiciona menu

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,11 @@ export default function App() {
     <header>
       <h1>Ola React! ✌️</h1>
       <hr />
+      <nav>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+      </nav>
     </header>
   );
 }
