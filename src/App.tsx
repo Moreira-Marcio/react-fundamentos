906be479c89f52db34c9ca939498985343adbe17
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-06-23 10:03:06 -0300

    atualiza com react fragmentação <> </>

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 export default function App() {
   return (
-    <header>
-      <h1>Ola React! ✌️</h1>
-      <hr />
-      <nav>
-        <a href=""></a>
-        <a href=""></a>
-        <a href=""></a>
-      </nav>
-    </header>
+    <>
+      <header>
+        <h1>Ola React! ✌️</h1>
+        <hr />
+        <nav>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+        </nav>
+      </header>
+      <main></main>
+    </>
   );
 }
