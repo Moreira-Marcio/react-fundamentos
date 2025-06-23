f2eccd76f9858b9d972249bb5a538e0bc3d89a52
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-06-23 11:15:09 -0300

    importar imagens para o cabeçalho

--- src/Cabecalho.tsx
+++ src/Cabecalho.tsx
@@ -1,7 +1,12 @@
+/* tudo o que for usado dentro de componentes (imagens, fontes,etc) deve ser importado desta forma, no momento do build havera otimizaçoes para esteconteudo  */
+import logo from "./assets/logo-react.png";
+
 export default function Cabecalho() {
   return (
     <header>
-      <h1>Ola React! ✌️</h1>
+      <h1>
+        <img src={logo} alt="" /> Ola React! ✌️
+      </h1>
       <hr />
       <nav>
         <a href=""></a>
