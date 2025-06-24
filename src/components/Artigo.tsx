0c8f98bed87a4f4790534cc69f1c48c945795555
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-06-24 09:15:26 -0300

    cria artigo dinamico

---
+++ src/components/Artigo.tsx
@@ -0,0 +1,11 @@
+export default function Artigo() {
+  return (
+    <article>
+      <h3>Artigo de exemplo 01</h3>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis
+        optio at fuga inventore accusamus eligendi!
+      </p>
+    </article>
+  );
+}
--- src/components/Conteudo.tsx
+++ src/components/Conteudo.tsx
@@ -1,30 +1,14 @@
+import Artigo from "./Artigo";
+
 export default function Conteudo() {
   return (
     <main>
       <section>
         <h2>Seja bem vindo aos fundamentos de react!!</h2>
         <p>Este é um exemplo de aplicação React.</p>
-        <article>
-          <h3>Artigo de exemplo 01</h3>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Perspiciatis optio at fuga inventore accusamus eligendi!
-          </p>
-        </article>
-        <article>
-          <h3>Artigo de exemplo 02</h3>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Perspiciatis optio at fuga inventore accusamus eligendi!
-          </p>
-        </article>
-        <article>
-          <h3>Artigo de exemplo 03</h3>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Perspiciatis optio at fuga inventore accusamus eligendi!
-          </p>
-        </article>
+        <Artigo />
+        <Artigo />
+        <Artigo />
       </section>
     </main>
   );
