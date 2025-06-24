ebd497deb4f93fdbb00f36d714f7fb61c92e9323
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-06-24 12:04:31 -0300

    cria exercicio style

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,8 @@
+import estiloNav from "./Menu.module.css";
+
 export default function Menu() {
   return (
-    <nav>
+    <nav className={estiloNav.links}>
       <a href="">Home</a>
       <a href="">Sobre</a>
       <a href="">Contato</a>
