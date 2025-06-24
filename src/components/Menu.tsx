83eb6b1f117e557f9ad13c06db02720fa7e39f5c
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-06-24 11:29:35 -0300

    atualiza menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,9 +1,9 @@
 export default function Menu() {
   return (
     <nav>
-      <a href=""></a>
-      <a href=""></a>
-      <a href=""></a>
+      <a href="">Home</a>
+      <a href="">Sobre</a>
+      <a href="">Contato</a>
     </nav>
   );
 }
