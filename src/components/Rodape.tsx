2fef339027ec7366ce124be7fa6fe46ef97fa70d
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-06-24 10:03:43 -0300

    aplica cssProprioeties

--- src/components/Rodape.tsx
+++ src/components/Rodape.tsx
@@ -1,7 +1,16 @@
+import type { CSSProperties } from "react";
+
+const estilosRodape: CSSProperties = {
+  backgroundColor: "#222",
+  color: "white",
+  textAlign: "center",
+  padding: "1rem",
+};
+
 export default function Rodape() {
   return (
     <>
-      <footer>
+      <footer style={estilosRodape}>
         <h2>Pequeno site criado com react</h2>
         <p style={{ color: "red", fontWeight: "bold" }}>
           desenvolvido por Marcio Moreira
