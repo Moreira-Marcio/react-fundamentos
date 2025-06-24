2eb48bb60374131978600b0e589a2893e6bb4de5
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-06-24 09:45:38 -0300

    cria e aplica class

--- src/components/Rodape.tsx
+++ src/components/Rodape.tsx
@@ -1,8 +1,11 @@
 export default function Rodape() {
   return (
-    <footer>
-      <h2>Pequeno site criado com react</h2>
-      <p>desenvolvido por Marcio Moreira</p>
-    </footer>
+    <>
+      <footer>
+        <h2>Pequeno site criado com react</h2>
+        <p>desenvolvido por Marcio Moreira</p>
+      </footer>
+      <p className="outro-texto">qualquer coisa...</p>
+    </>
   );
 }
