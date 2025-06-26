42f9b434cf6a79ec8fc2a3854c9b5c9d54c1c52d
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-06-26 10:27:39 -0300

    cria  desestrutura manipula e renderiza o props

--- src/components/Conteudo/Artigo/Artigo.tsx
+++ src/components/Conteudo/Artigo/Artigo.tsx
@@ -1,17 +1,27 @@
+import type { ReactNode } from "react";
 import estilos from "./Artigo.module.css";
 type ArtigoProps = {
   titulo: string;
-  texto: string;
   numero: number;
+  children: ReactNode;
+  data?: Date;
 };
 
-export default function Artigo({ titulo, texto, numero }: ArtigoProps) {
+export default function Artigo({
+  titulo,
+  numero,
+  children,
+  data,
+}: ArtigoProps) {
   return (
     <article className={estilos.artigo}>
       <h3>
         Artigo {numero}: {titulo}{" "}
       </h3>
-      <p>{texto}</p>
+
+      {data && <p>Data: {data?.toLocaleDateString()}</p>}
+
+      {children}
     </article>
   );
 }
--- src/components/Conteudo/Conteudo.tsx
+++ src/components/Conteudo/Conteudo.tsx
@@ -14,17 +14,32 @@ export default function Conteudo() {
         <Saudacao nome="Marcio" classe="bg-emerald-100" />
         <Saudacao classe="bg-red-100" />
         <p>Este é um exemplo de aplicação React.</p>
-        <Artigo
-          titulo="Front-End"
-          texto="texto qualquer sobre lfront"
-          numero={1}
-        />
-        <Artigo
-          titulo="Back-end"
-          texto="texto qualquer sobre back"
-          numero={2}
-        />
-        <Artigo titulo="Mobile" texto="texto qualque sobre mobile" numero={3} />
+        <Artigo titulo="Front-End" numero={1} data={new Date()}>
+          <p>
+            Front-end é muito bacana, a gente usa o html, css e o javascript
+          </p>
+          <p>texto qualquer sobre 3</p>
+        </Artigo>
+
+        <Artigo titulo="Back-end" numero={2}>
+          <p>É importante estudar</p>
+          <ul>
+            <li>PHP</li>
+            <li>SQL</li>
+            <li>Orientação objeto</li>
+            <p>e muito mais</p>
+          </ul>
+        </Artigo>
+
+        <Artigo titulo="Mobile" numero={3}>
+          <details>
+            <summary>Conheça as tecnologias mais usadas</summary>
+            <ol className="*:list-decimal *ml-7">
+              <li>JavaScript</li>
+              <li>Libs e Frameworks</li>
+            </ol>
+          </details>
+        </Artigo>
       </section>
 
       <DicaDoDia />
