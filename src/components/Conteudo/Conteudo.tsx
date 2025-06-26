08045b137ff7e0a46dd84b0a16b9200d7ef146de
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-06-26 08:48:07 -0300

    adiciona , usa e configura props

--- src/components/Conteudo/Conteudo.tsx
+++ src/components/Conteudo/Conteudo.tsx
@@ -10,7 +10,7 @@ export default function Conteudo() {
       <section
         className={`${estilos.principal} ${estilos.arredondada} ${estilos.sombra}`}
       >
-        <Saudacao />
+        <Saudacao nome="Marcio" />
         <p>Este é um exemplo de aplicação React.</p>
         <Artigo />
         <Artigo />
--- src/components/Conteudo/Saudacao.tsx
+++ src/components/Conteudo/Saudacao.tsx
@@ -1,7 +1,14 @@
-export default function Saudacao() {
+type Props = {
+  nome: string;
+};
+
+export default function Saudacao(props: Props) {
   return (
-    <h2 className="font-bold text-lg">
-      Seja bem vindo aos fundamentos de react!!
-    </h2>
+    <>
+      <h2 className="font-bold text-lg">fundamentos de react!!</h2>
+      <p className="my-2">
+        Bem-vindo(a) <b>{props.nome}</b>
+      </p>
+    </>
   );
 }
