eb02ecd62aca1d3e1e1e27df630036f983c792ca
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-06-27 09:05:31 -0300

    aplica prop evento com caliba

--- src/components/Cabecalho/Cabecalho.tsx
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -8,7 +8,7 @@ import estilos from "./Cabecalho.module.css";
 export default function Cabecalho() {
   return (
     <header className={estilos.topoSite}>
-      <h1 className={estilos.titulo}>
+      <h1 onClick={() => alert("ola!!")} className={estilos.titulo}>
         <img className={estilos.logo} src={logo} alt="" /> Ola React! ✌️
       </h1>
       <hr />
